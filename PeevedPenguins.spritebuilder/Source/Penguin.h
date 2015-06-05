c89d36c9534d0eb9d48b59e170e1c3ef2502e9e2
bearpaw2197 <bearpaw2197@outlook.com>
2015-06-05 16:36:06 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by Jay Bearpaw on 6/5/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
