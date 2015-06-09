2a1883f0a00c3e3ed5ad13b1193a0945858ba4be
bearpaw2197 <bearpaw2197@outlook.com>
2015-06-08 21:27:11 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -41,9 +41,6 @@
     
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
-
-     //visualize physics bodies & joints
-    _physicsNode.debugDraw = TRUE;
     
     _physicsNode.collisionDelegate = self;
     
